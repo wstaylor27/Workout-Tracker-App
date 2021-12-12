b0f760f840e96b31eec808c2b3d437e7759acd11
Zuke27 <wstaylor27@gmail.com>
2021-12-11 22:48:38 -0500

    Updated server.js

--- develop/server.js
+++ develop/server.js
@@ -1,19 +1,21 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(logger("dev"));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
 app.use(require("./public/api.js"));
+app.use(require("./public/index.js"));
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}!`);
